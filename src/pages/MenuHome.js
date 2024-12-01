c62137a88b51f43e582716e6690ddb187368446f
Mangatur Jeremi Silitonga <jeremimangatur21s08@gmail.com>
2024-12-01 15:33:53 +0700

    Fungsi card

--- src/pages/MenuHome.js
+++ src/pages/MenuHome.js
@@ -1,33 +1,21 @@
-import React, { useEffect } from "react";
-// import { Container } from "react-bootstrap";
-import { useNavigate, useLocation } from 'react-router-dom';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "../components/Header";
-
-import Section2 from "./Section2";
-import {FaSomeIcon} from 'react-icons/fa';
-import { BiBorderRadius } from "react-icons/bi";
 import ComponentName from "./zi";
+import Section2 from "./Section2";
 import Dashboard from './Dashboard';
 
-
 const MenuHome = () => {
-    const navigate = useNavigate();
-    const location = useLocation();
-
-    // useEffect(() => {
-    //     return () => {
-    //         document.body.style.background = "";
-    //     };
-    // }, []);
-
-    return (
-        <div>
-            <Navbar />
-            <ComponentName />
-            <Section2 />
-            <Dashboard />
-        </div>
-    );
+  return (
+    <div>
+      <Navbar />
+      <ComponentName />
+      <Section2 />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default MenuHome;
